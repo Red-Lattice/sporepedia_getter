4a74dd57934bcc950884f99a4ed315efdb70387c
unknown <err.error.found@gmail.com>
2023-11-02 21:43:39 -0400

    Fixed a bug where certain ID's would fail to create a URL

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,16 @@ fn get_range(start: u64, end: u64)
 {
     for i in start..=end
     {
+        // We don't need to clean the first id slice because it will always be more than 100
         let id_slice_1 = (i / 1000000000).to_string();
-        let id_slice_2 = ((i / 1000000) % 1000).to_string();
-        let id_slice_3 = ((i / 1000) % 1000).to_string();
 
-        //http://static.spore.com/static/thumb/501/104/218/501104218057.png
+        let id_slice_2_int = (i / 1000000) % 1000;
+        let id_slice_3_int = (i / 1000) % 1000;
+
+        let id_slice_2 = clean_id(id_slice_2_int);
+        let id_slice_3 = clean_id(id_slice_3_int);
+
+        //This is the format the URL's follow: http://static.spore.com/static/thumb/123/456/789/123456789123.png
         let file_name = format!("C:\\Users\\Ian\\projects\\sporepedia_getter\\png_pile\\{i}.png");
 
         let url = "http://static.spore.com/static/thumb/".to_owned() + &id_slice_1 
@@ -38,7 +43,7 @@ fn get_range(start: u64, end: u64)
         let img_size = std::fs::metadata(file_name.clone()).unwrap().len();
 
         // If a png is too small, it gets deleted because it's not a real creation
-        if img_size < 1000
+        if img_size < 500
         {
             let _ = fs::remove_file(file_name.clone());
         }
@@ -60,4 +65,17 @@ fn input_value() -> u64
         Err(..) => println!("this was not a valid ID: {}", trimmed),
     };
     return 500000000000;
+}
+
+fn clean_id(input: u64) -> String
+{
+    if input > 99
+    {
+        return input.to_string();
+    }
+    if input > 9
+    {
+        return "0".to_owned() + &input.to_string();
+    }
+    return "00".to_owned() + &input.to_string();
 }
