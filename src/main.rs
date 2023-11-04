349ada070e57d2d2c6edf3ac8e5a4eba99f28335
unknown <err.error.found@gmail.com>
2023-11-04 12:09:13 -0400

    Now works for any user, and creates necessary dirs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 extern crate reqwest;
 use std::fs;
 use std::io;
+use std::path::Path;
+
 
 fn main() 
 {
     //get_creator("MaxisCactus");
+    check_for_file();
     println!("Welcome to error/metalblaze/red lattice's sporepedia getter! Please enter a starting ID to begin your range");
     let start = input_value();
     println!("What ID would you like the search to end at? (inclusive)");
     let end = input_value();
     get_range(start, end);
     println!("Creations successfully gathered!");
+}
+
+fn check_for_file()
+{
+    fs::create_dir_all("png_pile");
 }
 
 fn get_range(start: u64, end: u64)
@@ -20,14 +28,12 @@ fn get_range(start: u64, end: u64)
         // We don't need to clean the first id slice because it will always be more than 100
         let id_slice_1 = (i / 1000000000).to_string();
 
-        let id_slice_2_int = (i / 1000000) % 1000;
-        let id_slice_3_int = (i / 1000) % 1000;
+        let id_slice_2 = clean_id((i / 1000000) % 1000);
+        let id_slice_3 = clean_id((i / 1000) % 1000);
 
-        let id_slice_2 = clean_id(id_slice_2_int);
-        let id_slice_3 = clean_id(id_slice_3_int);
-
+        let file_name_string = "png_pile//".to_owned() + &i.to_string() + ".png";
+        let file_name = Path::new(&file_name_string);
         //This is the format the URL's follow: http://static.spore.com/static/thumb/123/456/789/123456789123.png
-        let file_name = format!("C:\\Users\\Ian\\projects\\sporepedia_getter\\png_pile\\{i}.png");
 
         let url = "http://static.spore.com/static/thumb/".to_owned() + &id_slice_1 
             + "/" + &id_slice_2
@@ -46,7 +52,7 @@ fn get_range(start: u64, end: u64)
         // If a png is too small, it gets deleted because it's not a real creation
         if img_size < 500
         {
-            let _ = fs::remove_file(file_name.clone());
+            let _ = fs::remove_file(file_name);
         }
     }
 }
@@ -83,7 +89,7 @@ fn clean_id(input: u64) -> String
 
 fn get_creator(u_id: &str)
 {
-    let file_name = format!("C:\\Users\\Ian\\projects\\sporepedia_getter\\png_pile\\u_id.png");
+    let file_name = format!("C://Users//Ian//projects//sporepedia_getter//png_pile//u_id.png");
 
     let url = "http://www.spore.com/rest/assets/user/MaxisCactus/0/3";
 
