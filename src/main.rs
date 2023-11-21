42fab9f4e9e80fb8a1464222ffd955c374f40727
unknown <err.error.found@gmail.com>
2023-11-21 10:54:32 -0500

    Added asynchronous functionality and a progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use reqwest;
-use std::fs;
-use std::io;
-use std::path::Path;
+use std::{fs, io};
+use indicatif::{ProgressBar, ProgressStyle};
+use std::io::{BufRead, BufReader, Write};
 use std::{thread, time};
-use std::io::{BufRead, BufReader};
+use std::path::Path;
 use std::collections::HashSet;
 use std::fs::File;
-use std::io::Write;
+use reqwest::Client;
+use std::time::Duration;
+use reqwest;
 
-const MAX_ID:u64 = 501039850984;
+const CHECK_AT_ONCE:usize = 50;
 
-fn main() 
+#[tokio::main]
+async fn main() 
 {
     let mut big_list: Vec<File> = vec!();
     check_folder(Path::new("id_stack"), &mut big_list);
@@ -18,8 +20,22 @@ fn main()
     check_for_file();
     println!("\nWelcome to error/metalblaze/red lattice's sporepedia getter!");
     println!("\nInitializing...");
-    run(&hash_ids(big_list));
+    let hashed_ids = hash_ids(big_list);
+    run(&hashed_ids).await;
+    loop
+    {
+        if get_y_n_input()
+        {
+            run(&hashed_ids).await;
+        }
+        else
+        {
+            break;
+        }
+    }
     println!("\nProgram exited successfully");
+    let wait_period = time::Duration::from_secs(2);
+    thread::sleep(wait_period);
 }
 
 fn check_folder(parent_folder: &Path, big_list: &mut Vec<File>)
@@ -52,19 +68,15 @@ fn hash_ids(big_list: Vec<File>) -> HashSet<u64>
     return set;
 }
 
-fn run(valid_ids: &HashSet<u64>)
+async fn run(valid_ids: &HashSet<u64>)
 {
     println!("\nPlease enter a starting ID to begin your range");
     let start = input_value();
     println!("\nHow many ID's after this would you like to search? (inclusive)");
     let end = input_value();
-    get_range(start, end, valid_ids);
+    get_range(start, end, valid_ids).await;
     println!("\nCreations successfully gathered!");
     println!("\nWould you like to search another region? (Y/N)");
-    if get_y_n_input()
-    {
-        run(valid_ids);
-    }
     return;
 }
 
@@ -74,9 +86,7 @@ fn get_y_n_input() -> bool
 {
     let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
 
     let trimmed = input.trim();
     match trimmed
@@ -94,63 +104,79 @@ fn failed_y_n_input() -> bool
     return get_y_n_input();
 }
 
-fn get_range(start: u64, count: u64, valid_ids: &HashSet<u64>)
+static APP_USER_AGENT: &str = "Sporepedia Archival Team | contact at: err.error.found@gmail.com";
+
+async fn get_range(start: u64, count: u64, valid_ids: &HashSet<u64>)
 {
     let end = start + count;
-    for i in start..end
+
+    let bar = ProgressBar::new(count);
+    bar.set_style(ProgressStyle::with_template("[{elapsed_precise}] [{bar:40.blue/cyan}] {pos:>7}/{len:7} {msg} {percent}%   Estimated time remaining: {eta}")
+        .unwrap()
+        .progress_chars("##-"));
+
+    for i in (start..=end).step_by(CHECK_AT_ONCE)
     {
-        if i < MAX_ID
+        //This is the format the URL's follow: http://static.spore.com/static/thumb/123/456/789/123456789123.png
+        let mut ids_to_map: Vec<u64> = vec![];
+        for j in 0..=CHECK_AT_ONCE
         {
-            if !valid_ids.contains(&i)
+            if valid_ids.contains(&(i + j as u64))
             {
-                continue;
+                ids_to_map.push(i + j as u64);
+            }
+            else
+            {
+                bar.inc(1);
             }
         }
-        // We don't need to clean the first id slice because it will always be more than 100
-        let id_slice_1 = (i / 1000000000).to_string();
+        let urls = (0..ids_to_map.len()).map(|k| {
+            let url = url_builder(*ids_to_map.get(k).unwrap());
+            (url, k)
+        });
 
-        let id_slice_2 = clean_id((i / 1000000) % 1000);
-        let id_slice_3 = clean_id((i / 1000) % 1000);
-
-        let file_name_string = "png_pile//".to_owned() + &i.to_string() + ".png";
-        let file_name = Path::new(&file_name_string);
-        //This is the format the URL's follow: http://static.spore.com/static/thumb/123/456/789/123456789123.png
-
-        let url = "http://static.spore.com/static/thumb/".to_owned() + &id_slice_1 
-            + "/" + &id_slice_2
-            + "/" + &id_slice_3 
-            + "/" + &i.to_string() + ".png";
-
-        let mut buffer = vec![];
-
-        let result = reqwest::blocking::get(url.clone());
-                
-        let mut result = match result {
-            Ok(result) => result,
-            Err(_) => 
-            {
-                loop // If there was an error in fetching the request, it just retries until it works.
+        let results = futures::future::join_all(urls.map(|(url, k)|
+            async move 
+                { 
+                    let client = client_builder(); 
+                    (client.expect("REASON").get(url).send().await, k) 
+                }
+        )).await;
+        
+        for (result, k) in results.into_iter()
+        {
+            let result = match result {
+                Ok(result) => result,
+                Err(_) => 
                 {
-                    let result = reqwest::blocking::get(url.clone());
-                    if let Ok(result) = result {
-                        break result
+                    loop // If there was an error in fetching the request, it just retries until it works.
+                    {
+                        let url = url_builder(k as u64 + i);
+                        let result = reqwest::get(url.clone()).await;
+                        if let Ok(result) = result {
+                            break result
+                        }
                     }
                 }
+            };
+
+            let bytes = result.bytes().await.unwrap();
+            // If a png is too small, it gets deleted because it's not a real creation
+            if bytes.len() > 5000
+            {
+                let file_name_string = "png_pile//".to_owned() + &(k as u64 + i).to_string() + ".png";
+                let file_name = Path::new(&file_name_string);
+                let mut file = std::fs::File::create(file_name).unwrap();
+                file.write_all(&bytes).unwrap();
             }
-        };
-        let _ = result.copy_to(&mut buffer);
-
-        // If a png is too small, it gets deleted because it's not a real creation
-        if buffer.len() > 500
-        {
-            let mut file = std::fs::File::create(file_name).unwrap();
-            file.write_all(&buffer).unwrap();
+            bar.inc(1);
         }
-
-        // Rate limiting (We don't want to ddos the servers lmao)
-        let wait_period = time::Duration::from_millis(50);
-        thread::sleep(wait_period);
     }
+}
+
+fn client_builder() -> Result<Client, reqwest::Error>
+{
+    Client::builder().user_agent(APP_USER_AGENT).timeout(Duration::from_millis(15000)).build()
 }
 
 fn input_value() -> u64
@@ -167,9 +193,11 @@ fn input_value() -> u64
         Ok(i) =>  return i,
         Err(..) => println!("\nthis was not a valid ID: {}", trimmed),
     };
-    return 500000000000;
+    panic!();
 }
 
+/* Certain ID slices would have leading zeros, which would get removed when becoming a string.
+   This fixes it so that it has the leading zeroes to go along with it */
 fn clean_id(input: u64) -> String
 {
     if input > 99
@@ -181,4 +209,14 @@ fn clean_id(input: u64) -> String
         return "0".to_owned() + &input.to_string();
     }
     return "00".to_owned() + &input.to_string();
+}
+
+/* Builds a url to pull from */
+fn url_builder(input: u64) -> String
+{
+    // First ID slice doesn't need to be cleaned because it always has a leading 5 or 3
+    return "http://static.spore.com/static/thumb/".to_owned() + &(input / 1000000000).to_string() 
+    + "/" + &clean_id((input / 1000000) % 1000)
+    + "/" + &clean_id((input / 1000) % 1000)
+    + "/" + &input.to_string() + ".png";
 }
